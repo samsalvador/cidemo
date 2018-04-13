cdc9bc4ded6ed8cf0e2facd1e96558cfda88b125
Sam Salvador <35908848+samsalvador@users.noreply.github.com>
2018-04-13 08:56:35 +0800

    hey
    hey

---
+++ application/views/templates/footer.php
@@ -0,0 +1,32 @@
+ 
+
+			
+
+		 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		 	<div class="row">
+		 		<div class="col-sm-12">
+		 	<a class="navbar-brand" href="#" style="text-align:center;padding-left:400px">&copy; 2018</a><div style="text-align:left;padding-left:400px">
+		 		<br><br><h3 style="color:white">HOURS<br></h3>
+				<p>Monday — Sunday<br>
+				11.30am - 2.30pm<br>
+				5pm — 9pm<br><br></p>
+
+				<h3 style="color:white">LOCATION<br>
+				191 Riddiford Street ,Newtown, Wellington</h3><br>
+				<p>(04) 389 2263</p>
+				</div>
+				</div>
+			</div>
+
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarColor01">
+
+
+		  </div>
+		</nav>
+
+      </body>
+</html>
